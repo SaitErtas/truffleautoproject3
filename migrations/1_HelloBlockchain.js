56408facca9cf24cd7a09fd36b3c47066cbc72a7
SaitErtas <52601084+SaitErtas@users.noreply.github.com>
2022-11-17 00:32:53 +0300

    .

--- migrations/1_HelloBlockchain.js
+++ migrations/1_HelloBlockchain.js
@@ -1,10 +1,8 @@
-//const HelloBlockchain = artifacts.require("HelloBlockchain");
-//const DoubleMyContract = artifacts.require("DoubleMyContract");
+const HelloBlockchain = artifacts.require("HelloBlockchain");
+const DoubleMyContract = artifacts.require("DoubleMyContract");
 const ReceiveMoneyContract = artifacts.require("ReceiveMoneyContract");
-const MyToken2 = artifacts.require("MyToken2");
 
 module.exports = function (deployer) {
-  //deployer.deploy(HelloBlockchain, "Hello Blockchain");
-  //deployer.deploy(ReceiveMoneyContract, "ReceiveMoneyContract");
-  deployer.deploy(MyToken2, "MTK2");
+  deployer.deploy(HelloBlockchain, "Hello Blockchain");
+  deployer.deploy(ReceiveMoneyContract, "ReceiveMoneyContract3333333333333333");
 };
